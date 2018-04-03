de9c7f2dd22304891eb348ee5d1ee849c3ecb405
Akagi201 <akagi201@gmail.com>
2018-04-03 16:00:09 +0800

    feat(trade): add trade interface

--- contracts/chainex/chainex.cpp
+++ contracts/chainex/chainex.cpp
@@ -17,52 +17,15 @@ void chainex::withdraw(account_name from, extended_asset quantity) {
   currency::inline_transfer(_this_contract, from, quantity, "withdraw");
 }
 
-void chainex::createx(account_name creator, asset initial_supply, uint32_t fee,
-                      extended_asset base_deposit,
-                      extended_asset quote_deposit) {
-  require_auth(creator);
-  eosio_assert(initial_supply.is_valid(), "invalid initial supply");
-  eosio_assert(initial_supply.amount > 0, "initial supply must be positive");
-  eosio_assert(base_deposit.is_valid(), "invalid base deposit");
-  eosio_assert(base_deposit.amount > 0, "base deposit must be positive");
-  eosio_assert(quote_deposit.is_valid(), "invalid quote deposit");
-  eosio_assert(quote_deposit.amount > 0, "quote deposit must be positive");
-  eosio_assert(base_deposit.get_extended_symbol() !=
-                   quote_deposit.get_extended_symbol(),
-               "must exchange between two different currencies");
+void chainex::trade(account_name name, string side, uint128 price, string symbol, uint64 quantity, time expiration) {
+  require_auth(name);
+  
+}
 
-  print("base: ", base_deposit.get_extended_symbol());
-  print("quote: ", quote_deposit.get_extended_symbol());
-
-  auto exchange_symbol = initial_supply.symbol.name();
-  print("marketid: ", exchange_symbol, " \n");
-
-  markets exstates(_this_contract, exchange_symbol);
-  auto existing = exstates.find(exchange_symbol);
-
-  eosio_assert(existing == exstates.end(), "market already exists");
-  exstates.emplace(creator, [&](auto &s) {
-    s.manager = creator;
-    s.supply = extended_asset(initial_supply, _this_contract);
-    s.base.balance = base_deposit;
-    s.quote.balance = quote_deposit;
-  });
-
-  // TODO: After token contract respects maximum supply limits, the maximum
-  // supply here needs to be set appropriately.
-  _extokens.create(_this_contract, asset(0, initial_supply.symbol), false,
-                   false, false);
-
-  _extokens.issue(_this_contract, initial_supply,
-                  string("initial exchange tokens"));
-
-  _accounts.adjust_balance(creator,
-                           extended_asset(initial_supply, _this_contract),
-                           "new exchange issue");
-  _accounts.adjust_balance(creator, -base_deposit, "new exchange deposit");
-  _accounts.adjust_balance(creator, -quote_deposit, "new exchange deposit");
+void chainex::cancel_order(account_name name, string id) {
+  require_auth(name);
 }
 
 } // namespace eosio
 
-EOSIO_ABI(eosio::chainex, (deposit)(withdraw)(createx))
+EOSIO_ABI(eosio::chainex, (deposit)(withdraw))
--- contracts/chainex/chainex.hpp
+++ contracts/chainex/chainex.hpp
@@ -16,8 +16,9 @@ public:
 
   void withdraw(account_name from, extended_asset quantity);
 
-  void createx(account_name creator, asset initial_supply, uint32_t fee,
-               extended_asset base_deposit, extended_asset quote_deposit);
+  void trade(account_name name, string side, uint128 price, string symbol, uint64 quantity, time expiration);
+
+  void cancel_order(account_name name, string id);
 
 private:
   account_name _this_contract;
