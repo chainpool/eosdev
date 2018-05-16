6f10c69c9c6a9fa1a80ff9d48c2d695e59e49ee9
gguoss <guanghuaguo.oss@gmail.com>
2018-05-16 14:57:53 +0800

    Fix push implict transaction throw

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -665,7 +665,8 @@ struct controller_impl {
                FC_ASSERT(trx->trx.fee == txfee.get_required_fee(trx->trx), "set tx fee failed");
                try {
                   auto onftrx = std::make_shared<transaction_metadata>( get_on_fee_transaction(trx->trx.fee, trx->trx.actions[0].authorization[0].actor) );
-                  push_transaction( onftrx, fc::time_point::maximum(), true, config::default_min_transaction_cpu_usage_us);
+                  auto onftrace = push_transaction( onftrx, fc::time_point::maximum(), true, config::default_min_transaction_cpu_usage_us);
+                  if( onftrace->except ) throw *onftrace->except; 
                   ilog("-------call onfee function tx");
                } catch ( ... ) {
                   FC_ASSERT(false, "on fee transaction failed, but shouldn't enough asset to pay for transaction fee");
