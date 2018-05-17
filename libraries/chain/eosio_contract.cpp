2b26223a5bfe01859ec0846de8aae4681aea7dbf
Akagi201 <akagi201@gmail.com>
2018-05-17 16:34:43 +0800

    fix(governs): fix setcode get table

--- libraries/chain/eosio_contract.cpp
+++ libraries/chain/eosio_contract.cpp
@@ -127,13 +127,11 @@ static void copy_inline_row(const chain::key_value_object& obj, vector<char>& da
 
 // config::system_account_name
 
-bool allow_setcode(apply_context& context) {
+bool allow_setcode(apply_context& context, std::string code_id) {
   const auto& code_account = context.db.get<account_object,by_name>(N(eosio));
-  //const abi_def abi = get_abi( db, N(eosio) );
   chain::abi_def abi;
   if(abi_serializer::to_abi(code_account.abi, abi)) {
     abi_serializer abis(abi);
-    //get_table_rows_ex<key_value_index, by_scope_primary>(p,abi);
     const auto* t_id = context.db.find<chain::table_id_object, chain::by_code_scope_table>(boost::make_tuple(config::system_account_name, config::system_account_name, N(contractstat)));
     if (t_id != nullptr) {
       const auto &idx = context.db.get_index<key_value_index, by_scope_primary>();
@@ -141,24 +139,18 @@ bool allow_setcode(apply_context& context) {
       if (it != idx.end()) {
         vector<char> data;
         copy_inline_row(*it, data);
-        auto ct = abis.binary_to_variant("contract", data);
-        wlog("allow_setcode: ct: ${ct}", ("ct", ct));
-        return true;
+        auto ct = abis.binary_to_variant("contractstat_info", data);
+        auto& obj = ct.get_object();
+        auto code_obj = obj["code"].get_object();
+        auto cid = code_obj["code_id"].as_string();
+        if (cid == code_id) {
+          return true;
+        } else {
+          return false;
+        }
       }
     }
   }
-  // const auto iter = context.db_find_i64(N(eosio), N(eosio), N(contractstat), N(eosio));
-  // if (iter != -1) {
-  //   const auto buffer_size = context.db_get_i64(iter, nullptr, 0);
-  //   bytes value(buffer_size);
-  //   wlog("allow_setcode: buffer_size: ${bs}", ("bs", buffer_size));
-
-  //   const auto written_size = context.db_get_i64(iter, value.data(), buffer_size);
-  //   assert(written_size == buffer_size);
-  //   wlog("allow_setcode: data: ${data}", ("data", value));
-
-  //   return true;
-  // }
   return false;
 }
 
@@ -194,7 +186,7 @@ void apply_eosio_setcode(apply_context& context) {
    // Not first time setcode
    if (account.code_version != fc::sha256::sha256()) {
      // get allow_setcode from system contract table
-     if (!allow_setcode(context)) {
+     if (!allow_setcode(context, code_id.str())) {
        // exit
        FC_THROW("The code_id '${code_id}' is not approved by the system contract", ("code_id", code_id));
      }
