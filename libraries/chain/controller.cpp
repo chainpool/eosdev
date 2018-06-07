0f9ee2417f2bf57c45124bd1f7ed517e3528cdb7
icodezjb <icodezjb@163.com>
2018-06-07 16:09:26 +0800

    update for System contract

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -382,7 +382,8 @@ struct controller_impl {
          obj.name = name;
          obj.producer_key = public_key;
          obj.commission_rate = producer.commission_rate;
-         obj.voteage_update_time = conf.genesis.initial_timestamp;
+         obj.total_staked = 0;
+         obj.voteage_update_height = 0;
          obj.total_voteage = 0;
          obj.emergency = false;
          auto pk = obj.primary_key();
