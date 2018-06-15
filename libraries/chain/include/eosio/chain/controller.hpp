5fc37178a9fccb669f5578dc7dfc705338105cc6
icodezjb <icodezjb@163.com>
2018-06-15 19:02:30 +0800

    update check_action

--- libraries/chain/include/eosio/chain/controller.hpp
+++ libraries/chain/include/eosio/chain/controller.hpp
@@ -40,6 +40,13 @@ namespace eosio { namespace chain {
        asset                            quantity;
        string                           memo;
    };
+
+   struct tmp_issue {
+       account_name                     to;
+       asset                            quantity;
+       string                           memo;
+   };
+
    struct tmp_vote {
        account_name                     voter;
        account_name                     bpname;
@@ -252,6 +259,7 @@ namespace eosio { namespace chain {
 } }  /// eosio::chain
 
 FC_REFLECT( eosio::chain::tmp_transfer, (from)(to)(quantity)(memo) )
+FC_REFLECT( eosio::chain::tmp_issue, (to)(quantity)(memo) )
 FC_REFLECT( eosio::chain::tmp_vote, (voter)(bpname)(change) )
 FC_REFLECT( eosio::chain::tmp_claim, (voter)(bpname) )
 FC_REFLECT( eosio::chain::tmp_unfreeze, (voter)(bpname))
